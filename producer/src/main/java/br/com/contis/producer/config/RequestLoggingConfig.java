729ca9344c0af0cdfdbbdff5f7f60279c9114562
Felipe Oliveira <lipefan0@gmail.com>
2025-05-15 10:14:39 -0300

    feat: adiciona prioridade no log

--- producer/src/main/java/br/com/contis/producer/config/RequestLoggingConfig.java
+++ producer/src/main/java/br/com/contis/producer/config/RequestLoggingConfig.java
@@ -1,20 +1,26 @@
 package br.com.contis.producer.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class RequestLoggingConfig {
 
     @Bean
-    public CommonsRequestLoggingFilter logFilter() {
+    public FilterRegistrationBean<CommonsRequestLoggingFilter> loggingFilter(){
         CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
         filter.setIncludeClientInfo(true);
         filter.setIncludeQueryString(true);
         filter.setIncludePayload(true);
         filter.setMaxPayloadLength(10_000);
-        filter.setIncludeHeaders(false);  // true se quiser headers
-        return filter;
+        filter.setIncludeHeaders(false);
+
+        FilterRegistrationBean<CommonsRequestLoggingFilter> bean =
+                new FilterRegistrationBean<>(filter);
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);   // << registra antes de tudo
+        return bean;
     }
 }
