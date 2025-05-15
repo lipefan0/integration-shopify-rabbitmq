45b62842caad846a9d34d4436162f39917c66006
Felipe Oliveira <lipefan0@gmail.com>
2025-05-15 10:04:05 -0300

    config: adiciona filtro para verificar o que está entrando na minha rota

---
+++ producer/src/main/java/br/com/contis/producer/config/RequestLoggingConfig.java
@@ -0,0 +1,20 @@
+package br.com.contis.producer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10_000);
+        filter.setIncludeHeaders(false);  // true se quiser headers
+        return filter;
+    }
+}
